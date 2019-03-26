ce40fb983ceb40744180950301663ff6121800f8
albertec1 <45201952+albertec1@users.noreply.github.com>
2019-03-26 10:31:32 +0100

    handout 4

---
+++ SDL4_Handout/ModuleBackground.cpp
@@ -0,0 +1,69 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleBackground.h"
+
+// Reference at https://www.youtube.com/watch?v=OEhmUuehGOA
+
+ModuleBackground::ModuleBackground()
+{
+	// ground
+	ground.x = 8;
+	ground.y = 391;
+	ground.w = 896;
+	ground.h = 72;
+
+	// Background / sky
+	background.x = 72;
+	background.y = 208;
+	background.w = 768;
+	background.h = 176;
+
+	// flag animation
+	flag.PushBack({848, 208, 40, 40});
+	flag.PushBack({848, 256, 40, 40});
+	flag.PushBack({848, 304, 40, 40});
+	flag.speed = 0.08f;
+
+	// ship animation
+	ship.x = 30;
+	ship.y = 30;
+	ship.h = 180;
+	ship.w = 522;
+	
+
+}
+
+ModuleBackground::~ModuleBackground()
+{}
+
+// Load assets
+bool ModuleBackground::Start()
+{
+	
+	LOG("Loading background assets");
+	bool ret = true;
+	graphics = App->textures->Load("ken_stage.png");
+	return ret;
+}
+
+// Update: draw background
+update_status ModuleBackground::Update()
+{
+	// Draw everything --------------------------------------
+	App->render->Blit(graphics, 0, 0, &background, 0.75f); // sea and sky
+	
+	App->render->Blit(graphics, 560, 8, &(flag.GetCurrentFrame()), 0.75f); // flag animation
+	// TODO 2: Draw the ship from the sprite sheet with some parallax effect
+	
+	frameCounter++;
+	if (frameCounter == 5 && yPosFlag 5) { yPosFlag -= 1; frameCounter = 0; }
+	if (frameCounter == 5 && yPosFlag  0) { yPosFlag += 1; frameCounter = 0; }
+	App->render->Blit(graphics, 0, yPosFlag, &ship, 0.75f); //ship
+	// TODO 3: Animate the girl on the ship (see the sprite sheet)
+
+	App->render->Blit(graphics, 0, 170, &ground);
+
+	return UPDATE_CONTINUE;
+}
